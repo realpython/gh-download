9fe08a819d70aac30fe77fa84ae0c7f4e3002625
Ian <iansedano@gmail.com>
2022-06-12 13:07:50 +0200

    change buttons to links

--- src/initializeButtons.js
+++ src/initializeButtons.js
@@ -1,15 +1,7 @@
-import { RP_HOME } from "./query.js";
-
 export function initializeButtons(downloadCallback, sourceCodeLink) {
   document
     .getElementById("download")
     .addEventListener("click", downloadCallback);
 
-  document.getElementById("src-code").addEventListener("click", () => {
-    location.href = sourceCodeLink;
-  });
-
-  document.getElementById("back-to-rp").addEventListener("click", () => {
-    location.href = RP_HOME;
-  });
+  document.getElementById("src-code").href = sourceCodeLink;
 }
