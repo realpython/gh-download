2f39937e2cae079f4b95eeb8922be0256142e633
Ian <iansedano@gmail.com>
2022-06-20 16:29:21 +0200

    fixing source code new window bug

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
-import { MaterialsQuery } from "./query.js";
+import { MaterialsQuery, FALLBACK_URL } from "./query.js";
 
 import { hydrateDownloadButton, hydrateSourceCodeButton } from "./buttons.js";
 import { errorScreen } from "./errorScreen.js";
 import { ERROR_TYPE } from "./error.js";
 
-const MATERIALS_REPO = "https://github.com/realpython/materials";
-
 window.onload = async () => {
   try {
     const materialsQuery = new MaterialsQuery();
-    const srcCodeLink = materialsQuery.getSourceCodeLink() || MATERIALS_REPO;
+    const srcCodeLink = materialsQuery.getSourceCodeLink();
     hydrateSourceCodeButton(srcCodeLink);
-    if (materialsQuery.downloadCallback) {
-      hydrateDownloadButton(materialsQuery.downloadCallback);
-      await materialsQuery.download();
-    } else {
-      throw new Error("Not downloadable");
-    }
+    hydrateDownloadButton(materialsQuery.downloadCallback);
+    await materialsQuery.download();
   } catch (e) {
-    hydrateSourceCodeButton(MATERIALS_REPO);
     if (e.message == ERROR_TYPE.API_LIMIT) {
       errorScreen(ERROR_TYPE.API_LIMIT);
     } else if (e.message == ERROR_TYPE.NOT_FOUND) {
       errorScreen(ERROR_TYPE.NOT_FOUND);
-      hydrateSourceCodeButton(MATERIALS_REPO, true);
     } else {
       console.log(e);
       errorScreen();
